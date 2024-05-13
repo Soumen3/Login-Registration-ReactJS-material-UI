77adfe6072c9ffeca1fcf57b3a380bbcad53351e
Soumen3 <soumensamanta112233@gmail.com>
2024-05-13 17:35:56 +0530

    design

--- src/components/pages/auth/LoginReg.jsx
+++ src/components/pages/auth/LoginReg.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Grid, Card, Tabs, Tab, Box} from "@mui/material";
+import { Grid, Card, Tabs, Tab, Box, Typography} from "@mui/material";
 import Pic1 from "../../../images/pic1.png";
 import UserLogin from "./UserLogin";
 import Registration from "./Registration";
+import { ShoppingBag } from "@mui/icons-material";
 
 const TabPanel = (props) => {
 	const {children, value, index, ...other} = props;
@@ -33,7 +34,7 @@ function LoginReg() {
 			</Grid>
 			<Grid item lg={5} sm={7} xs={12}>	
 				<Card sx={{width:'100%', height:'100%'}}>
-					<Box>
+					<Box sx={{mx:3, height: 530}}>
 						<Box sx={{borderBottom: 1, borderColor:'divider'}}>
 							<Tabs value={value} textColor="secondary" indicatorColor="secondary" onChange={handleChange}>
 								<Tab label="Login" sx={{textTransform:'none', fontWeight:'bold'}}/>
@@ -43,6 +44,10 @@ function LoginReg() {
 							<TabPanel value={value} index={1}> <Registration /> </TabPanel>
 						</Box>
 					</Box>
+					<Box textAlign='center' sx={{mt:2}}>
+							<ShoppingBag sx={{color: 'purple', fontSize: 100}} ></ShoppingBag>
+							<Typography variant="h5" sx={{fontWeight:'bold'}} >Geek-Shop</Typography>
+						</Box>
 				</Card>
 			</Grid>
 		</Grid>
